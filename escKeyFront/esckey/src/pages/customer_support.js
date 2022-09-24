acc190c9e6aec6d48dedf8f2fda36af310f94957
Pyotato <pyolovely01@gmail.com>
2022-09-24 10:26:06 +0900

    implement color theme change function for customer support page

--- escKeyFront/esckey/src/pages/customer_support.js
+++ escKeyFront/esckey/src/pages/customer_support.js
@@ -1,7 +1,7 @@
 import Footer from "../components/footer/footer";
 import Header from "../components/header/header";
 import styled from "styled-components";
-import { colors, fontSizes, margins, paddings } from "../theme/theme";
+import { colors, fontSizes, margins, paddings, theme } from "../theme/theme";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCommentDots } from "@fortawesome/free-regular-svg-icons";
@@ -9,6 +9,7 @@ import { faQuestion } from "@fortawesome/free-solid-svg-icons";
 
 const CustomerSupportPage = () => {
   const navigate = useNavigate();
+
   return (
     <>
       <Header />
@@ -17,28 +18,16 @@ const CustomerSupportPage = () => {
         <div>무엇을 도와드릴까요?</div>
         <div className="flexWrap">
           <div className="hoverIcon">
-            <img
-              className="hoverIcon"
-              src="/img/icon/arcticons_user_manual.png"
-              alt="faq_icon"
+            <div
+              className="hoverIcon user_manual_dark"
               onClick={() => navigate("/customizing/instructions")}
             />
           </div>
           <div>
-            <img
-              className="hoverIcon"
-              src="/img/icon/FAQ.png"
-              alt="faq_icon"
-              onClick={() => navigate("/FAQ")}
-            />
+            <div className="hoverIcon faq" onClick={() => navigate("/FAQ")} />
           </div>
           <div>
-            <img
-              className="hoverIcon"
-              src="/img/icon/chatbot.png"
-              alt="faq_icon"
-              onClick={() => navigate("/chat")}
-            />
+            <div className="hoverIcon chat" onClick={() => navigate("/chat")} />
           </div>
         </div>
       </CustomerSupportWrap>
@@ -53,21 +42,44 @@ const CustomerSupportWrap = styled.div`
     theme.theme === "dark" ? colors.color_bg_dark : colors.color_bg_white};
   width: 100%;
   font-size: ${fontSizes.fontSize_xxxl};
-  padding: 12%;
+  padding: 9%;
+
   & > div {
     color: ${({ theme }) =>
-      theme.theme === "dark" ? colors.color_bg_dark : colors.color_bg_white};
+      theme.theme === "dark" ? colors.color_bg_dark : colors.color_gray_red};
   }
-
+  & .user_manual_dark {
+    background-image: ${({ theme }) =>
+      theme.theme === "dark"
+        ? "url(/img/icon/user_manual_dark.png)"
+        : "url(/img/icon/user_manual_light.png)"};
+    background-repeat: no-repeat;
+    width: 9em;
+    height: 12em;
+  }
+  & .faq {
+    background-image: ${({ theme }) =>
+      theme.theme === "dark"
+        ? "url(/img/icon/FAQ_dark.png)"
+        : "url(/img/icon/FAQ_light.png)"};
+    background-repeat: no-repeat;
+    width: 9em;
+    height: 12em;
+  }
+  & .chat {
+    background-image: ${({ theme }) =>
+      theme.theme === "dark"
+        ? "url(/img/icon/chatbot_dark.png)"
+        : "url(/img/icon/chatbot_light.png)"};
+    background-repeat: no-repeat;
+    width: 9em;
+    height: 12em;
+  }
   & .flexWrap {
     display: inline-flex;
-    gap: 12em;
+    gap: 9em;
     padding: ${paddings.padding_uuul};
     width: 100%;
-  }
-
-  & .hoverIcon {
-    width: ${fontSizes.fontSize_xxlTitleSize};
   }
 
   & .hoverIcon:hover {
